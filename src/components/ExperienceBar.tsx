a08ff90a622f4d3b21b9785759d498aceb932d7e
dclleite <eduappsplay@gmail.com>
2021-03-07 12:28:44 -0300

    experienceBar component

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,20 +1,28 @@
-import styles from '../styles/components/ExperienceBar.module.css';
+import { useChallenges } from "../hooks/useChallenges";
+import styles from "../styles/components/ExperienceBar.module.css";
 
 export function ExperienceBar() {
-	return (
-		<header className={styles.experienceBar}>
-			<span>0 px</span>
-			<div>
-				<div style={{ width: '50%' }} />
+  const { currentExperience, experienceToNextLevel } = useChallenges();
 
-				<span
-					className={styles.currentExperience}
-					style={{ left: '50%' }}
-				>
-					300 xp
-				</span>
-			</div>
-			<span>600 px</span>
-		</header>
-	);
+  const percentToNextLevel =
+    Math.round(currentExperience * 100) / experienceToNextLevel;
+
+  return (
+    <header className={styles.experienceBar}>
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: `${percentToNextLevel}%` }} />
+
+        {currentExperience > 0 && (
+          <span
+            className={styles.currentExperience}
+            style={{ left: `${percentToNextLevel}%` }}
+          >
+            {currentExperience} xp
+          </span>
+        )}
+      </div>
+      <span>{experienceToNextLevel} xp</span>
+    </header>
+  );
 }
