3dc12ea70f8ea52cda017f3f1e236127284a50d7
dclleite <eduappsplay@gmail.com>
2021-03-07 12:29:26 -0300

    profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,15 +1,20 @@
-import styles from '../styles/components/Profile.module.css';
+import { useChallenges } from "../hooks/useChallenges";
+
+import styles from "../styles/components/Profile.module.css";
+
 export function Profile() {
-	return (
-		<div className={styles.profileContainer}>
-			<img src='https://github.com/dclleite.png' alt='Eduardo Leite' />
-			<div>
-				<strong>Eduardo Leite</strong>
-				<p>
-					<img src='icons/level.svg' alt='Level' />
-					Level 1
-				</p>
-			</div>
-		</div>
-	);
+  const { level } = useChallenges();
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/dclleite.png" alt="Eduardo Leite" />
+      <div>
+        <strong>Eduardo Leite</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
 }
